db143db35c7179769c31835f334f83c2cdec9f41
DilanMC22 <dilanmenachaves@gmail.com>
2025-11-19 11:37:46 -0600

    Update layoutInterno.php
    Agregada opción Productos al menú lateral

--- View/layoutInterno.php
+++ View/layoutInterno.php
@@ -67,29 +67,36 @@
         ';
     }
 
-    function showSideBar(){
-        echo '
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Core</div>
+   function showSideBar(){
+    echo '
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Core</div>
 
-                            <a class="nav-link" href="Principal.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
-                            </a>
+                        <a class="nav-link" href="Principal.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Dashboard
+                        </a>
 
-                        </div>
+                        <!-- NUEVA OPCIÓN -->
+                        <a class="nav-link" href="../productos.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-box"></i></div>
+                            Productos
+                        </a>
+
                     </div>
-                    <div class="sb-sidenav-footer">
-                        <div class="small">Sesión como:</div>
-                        ' . $_SESSION["Nombre"] . '
-                    </div>
-                </nav>
-            </div>
-        ';
-    }
+                </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Sesión como:</div>
+                    ' . $_SESSION["Nombre"] . '
+                </div>
+            </nav>
+        </div>
+    ';
+}
+
 
     function showFooter(){
         echo '
